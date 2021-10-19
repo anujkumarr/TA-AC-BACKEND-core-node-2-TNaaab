fb9c24d4db262e2b16e8d0614b2a167cbcdac676
akk26 <anujbph@gmail.com>
2021-10-19 22:35:05 +0530

    Refactor

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -6,23 +6,25 @@ var server = http.createServer(handleRequest);
 function handleRequest(req, res) {
   var dataFormat = req.headers['content-type'];
   var store = '';
-  if (req.method === 'POST' && req.url === "/") {
-    req.on('data', (chunk) => {
-      store = store + chunk;
-    });
+  req.on('data', (chunk) => {
+    store = store + chunk;
+  });
+  
+  req.on('end', () => {
+    if (req.method === 'POST' && req.url === '/json') {
+      res.setHeader('Content-Type', 'application/json')
+      var parsedData = JSON.parse(store);
+      res.end(JSON.stringify(parsedData));
+    }
 
-    req.on('end', () => {
-      if (dataFormat === 'application/json') {
-        var parsedData = JSON.parse(store);
-        res.end(JSON.stringify(parsedData));
-      }
-
-      if (dataFormat === 'application/x-www-form-urlencoded') {
-        var parsedData = qs.parse(store);
-        res.end(JSON.stringify(parsedData));
-      }
-    });
-  }
+    if (
+      req.method === 'POST' && req.url === '/form'
+    ) {
+      res.setHeader('Content-Type', 'application/x-www-form-urlencoded');
+      var parsedData = qs.parse(store);
+      res.end(JSON.stringify(parsedData));
+    }
+  });
 }
 
 server.listen(7000, () => {
